954a1839a46dd7f4596a90e43e493dd8c399bbbe
Natraj Padwani <nextronickk@gmail.com>
2024-03-27 07:22:10 +0800

    Added some hints on what I can do to make the extension better

--- contentScript.js
+++ contentScript.js
@@ -16,14 +16,39 @@ chrome.storage.sync.get(
 );
 
 function applyDarkTheme() {
+  //p, h1, h2, h3, h4, h5, h6
   const darkStyles = `
-    body, div, p, h1, h2, h3, h4, h5, h6 {
+  *, html, body {
+    font-family: sans-serif;
+  }
+    body, div {
       background-color: #121212 !important;
       color: #ffffff !important;
     }
     /* Add more styles as needed */
+    a {
+        color: #cccccc !important;
+    }
+    a:hover {
+        color: #ffffff !important;
+    }
+    a:focus {
+        color: #dddddd !important;
+    }
+    input[type="text"] {
+        background-color: white !important;
+        color: #000000 !important;
+    }
+    input[type="radio"]{
+        background-color: gray !important;
+        color: black !important;
+    }
+    .active input[type="radio"]{
+        background-color: white !important;
+        color: black !important;
+    }
   `;
-
+  //TODO: loop through elements and go with the oposite style when it comes to color but for background make it gray if its not white color
   const styleElement = document.createElement("style");
   styleElement.textContent = darkStyles;
   document.head.appendChild(styleElement);
